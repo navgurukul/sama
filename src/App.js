370c39fe5042436617ee9882a14797435b591d8a
amansharmma <aman18@navgurukul.org>
2024-07-23 18:16:22 +0530

    Added the theme and footer.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { ThemeProvider } from "@mui/material/styles";
+import theme from './theme/theme';
 import Navbar from './components/Navbar';
-// import Footer from './components/Footer';
+import Footer from './components/Footer';
 import Home from './components/Home';
-// import About from './components/About';
+// import About from './components/About';  
 // import Projects from './components/Projects';
 // import Contact from './components/Contact';
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      <Navbar />
+   
+    <ThemeProvider theme={theme}>
+     <Router>
+      <Navbar/>
+      
       <Routes>
         <Route path="/" element={<Home />} />
         {/* <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/contact" element={<Contact />} /> */}
       </Routes>
-      {/* <Footer /> */}
+      <Footer/>
     </Router>
+  </ThemeProvider>
   );
 }
 
