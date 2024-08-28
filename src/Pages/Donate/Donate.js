5fa33cfa20799d2af847af7625429bcc43589396
Your Name <your.email@example.com>
2024-08-28 13:27:01 +0530

    emaildata

--- src/Pages/Donate/Donate.js
+++ src/Pages/Donate/Donate.js
@@ -123,7 +123,7 @@ function Donation() {
     } else {
       try {
         const response = await fetch(
-          "https://script.google.com/macros/s/AKfycbzpJM1YwhElNojDizOb1ksc56XxwfWuZzpz-T3A9Tw9vh90XIAP5E__FQZT-idBRY4DBw/exec",
+        "https://script.google.com/macros/s/AKfycbxvHdTYd1lfVq_Y3r4w873GXwCFXP5MowPke5q8eWayEto81IxgtSQ37efu2s9I8zd5bg/exec",
           {
             method: "POST",
             headers: {
