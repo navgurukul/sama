8952a25e8f4284f8043deeab9055939c81b91b76
ng <khushboo kumari>
2024-10-23 15:51:03 +0530

    style

--- src/Dashboard/AdminNgo/index.js
+++ src/Dashboard/AdminNgo/index.js
@@ -1,19 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import {
-  Container, Typography, Grid, TextField, MenuItem, 
-  Select, FormControl, InputLabel, Paper, Table, 
-  TableBody, TableCell, TableContainer, TableHead, 
-  TableRow, Button, IconButton, Dialog, DialogActions, 
-  DialogContent, DialogContentText, DialogTitle
+  Container, Typography, Grid, TextField, MenuItem,
+  Select, FormControl, InputLabel, Paper, Table,
+  TableBody, TableCell, TableContainer, TableHead,
+  TableRow, Button, IconButton, Dialog, DialogActions,
+  DialogContent, DialogContentText, DialogTitle, Box
 } from '@mui/material';
-import DeleteIcon from '@mui/icons-material/Delete'; 
+import FilterListIcon from '@mui/icons-material/FilterList';
+import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
 import SaveIcon from '@mui/icons-material/Save';
 import axios from 'axios';
+import TablePagination from '@mui/material/TablePagination';
+import Divider from '@mui/material/Divider';
 import { useNavigate } from 'react-router-dom';
 
+import InputAdornment from '@mui/material/InputAdornment';
+import SearchIcon from '@mui/icons-material/Search';
+import { classes } from './style';
+
 const AdminNgo = () => {
-  const navigate = useNavigate(); 
+  const navigate = useNavigate();
   const [ngoData, setNgoData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [filters, setFilters] = useState({
@@ -23,6 +30,8 @@ const AdminNgo = () => {
     status: '',
   });
   const [searchTerm, setSearchTerm] = useState('');
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
 
   const [filterOptions, setFilterOptions] = useState({
     laptopsRequired: [],
@@ -42,7 +51,7 @@ const AdminNgo = () => {
         const response = await axios.get('https://script.google.com/macros/s/AKfycbxm2qA0DvzVUNtbwe4tAqd40hO7NpNU-GNXyBq3gHz_q45QIo9iveYOkV0XqyfZw9V7/exec?type=registration');
         const data = response.data.data;
 
-        setNgoData(data); 
+        setNgoData(data);
         setLoading(false);
 
         const laptopsRequiredOptions = [...new Set(data.map(item => item.primaryContactName))];
@@ -97,14 +106,14 @@ const AdminNgo = () => {
   };
 
   const handleConfirmStatusChange = async (e) => {
-    const updatedData = ngoData.map((ngo) => 
+    const updatedData = ngoData.map((ngo) =>
       ngo.Id === ngoIdToChange ? { ...ngo, status: selectedStatus } : ngo
     );
     setNgoData(updatedData);
     setOpenDialog(false); // Close dialog
 
     // Call API to save status
-    
+
     try {
       const response = await fetch('https://script.google.com/macros/s/AKfycbxm2qA0DvzVUNtbwe4tAqd40hO7NpNU-GNXyBq3gHz_q45QIo9iveYOkV0XqyfZw9V7/exec', {
         method: "POST",
@@ -136,8 +145,8 @@ const AdminNgo = () => {
 
   const filteredData = ngoData.filter((ngo) =>
     (ngo.organizationName?.toLowerCase().includes(searchTerm) ||
-     ngo.location?.toLowerCase().includes(searchTerm) ||
-     ngo.contactNumber?.toString().includes(searchTerm))
+      ngo.location?.toLowerCase().includes(searchTerm) ||
+      ngo.contactNumber?.toString().includes(searchTerm))
     && (filters.laptopsRequired ? ngo.primaryContactName === filters.laptopsRequired : true)
     && (filters.purpose ? ngo.expectedOutcome === filters.purpose : true)
     && (filters.location ? ngo.location === filters.location : true)
@@ -145,28 +154,44 @@ const AdminNgo = () => {
   );
 
   return (
-    <Container maxWidth="xl">
-      <Typography variant="h4" gutterBottom>All NGOs (100)</Typography>
-
-      {/* Filters Section */}
-      <TextField
-        label="Search by Name, Location, Contact"
-        variant="outlined"
-        maxWidth="lg"
-        value={searchTerm}
-        onChange={handleSearchChange}
-      />
+    <Container maxWidth="xxl">
+      <Typography variant="h6" gutterBottom>All NGOs (100)</Typography>
       <Grid container spacing={2}>
-        <Grid item xs={6} sm={1} md={1}>
-          <Typography variant="subtitle1">Filters</Typography>
+        <Grid item xs={12} sm={6} md={3} sx={{ mt: 3 }}>
+          <TextField
+            sx={{
+              width: {
+                lg: "480px",
+                sm: "100%",
+                xs:"100%"
+              }
+            }}
+            label="Search by Name, Location, Contact"
+            variant="outlined"
+            maxWidth="lg"
+            value={searchTerm}
+            onChange={handleSearchChange}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <SearchIcon />
+                </InputAdornment>
+              ),
+            }}
+          />
         </Grid>
-        <Grid item xs={12} sm={6} md={2}>
-          <FormControl fullWidth variant="outlined">
-            <InputLabel>Laptops Required</InputLabel>
+      </Grid>
+      <Grid container spacing={2} sx={{ mt: 2 }}>
+        <Box sx={{ display: "flex" }}>
+          < FilterListIcon sx={{ mt: 3, ml: 2 }} />
+          <Typography variant="subtitle1" sx={classes.filter}>Filters</Typography>
+        </Box>
+        <div>
+          <FormControl required sx={classes.FormControl}>
+            <InputLabel id="demo-simple-select-required-label">Laptops Required</InputLabel>
             <Select
-              value={filters.laptopsRequired}
-              onChange={handleFilterChange}
-              name="laptopsRequired"
+              labelId="demo-simple-select-required-label"
+              id="demo-simple-select-required"
               label="Laptops Required"
             >
               <MenuItem value=""><em>None</em></MenuItem>
@@ -175,14 +200,11 @@ const AdminNgo = () => {
               ))}
             </Select>
           </FormControl>
-        </Grid>
-        <Grid item xs={12} sm={6} md={2}>
-          <FormControl fullWidth variant="outlined">
-            <InputLabel>Purpose</InputLabel>
+          <FormControl required sx={classes.FormControl}>
+            <InputLabel id="demo-simple-select-required-label">Purpose</InputLabel>
             <Select
-              value={filters.purpose}
-              onChange={handleFilterChange}
-              name="purpose"
+              labelId="demo-simple-select-required-label"
+              id="demo-simple-select-required"
               label="Purpose"
             >
               <MenuItem value=""><em>None</em></MenuItem>
@@ -191,15 +213,12 @@ const AdminNgo = () => {
               ))}
             </Select>
           </FormControl>
-        </Grid>
-        <Grid item xs={12} sm={6} md={2}>
-          <FormControl fullWidth variant="outlined">
-            <InputLabel>Location</InputLabel>
+          <FormControl sx={{...classes.FormControl,width:"230px"}}>
+            <InputLabel id="demo-simple-select-readonly-label">Location of Operation</InputLabel>
             <Select
-              value={filters.location}
-              onChange={handleFilterChange}
-              name="location"
-              label="Location"
+              label="Location of Operation"
+              labelId="demo-simple-select-readonly-label"
+              id="demo-simple-select-readonly"
             >
               <MenuItem value=""><em>None</em></MenuItem>
               {filterOptions.location.map(option => (
@@ -207,14 +226,11 @@ const AdminNgo = () => {
               ))}
             </Select>
           </FormControl>
-        </Grid>
-        <Grid item xs={12} sm={6} md={2}>
-          <FormControl fullWidth variant="outlined">
-            <InputLabel>Status</InputLabel>
+          <FormControl required sx={classes.FormControl}>
+            <InputLabel id="demo-simple-select-required-label">Status</InputLabel>
             <Select
-              value={filters.status}
-              onChange={handleFilterChange}
-              name="status"
+              labelId="demo-simple-select-required-label"
+              id="demo-simple-select-required"
               label="Status"
             >
               <MenuItem value=""><em>None</em></MenuItem>
@@ -223,51 +239,57 @@ const AdminNgo = () => {
               ))}
             </Select>
           </FormControl>
-        </Grid>
+        </div>
       </Grid>
 
-      {/* Custom Table */}
-      <TableContainer component={Paper} sx={{ backgroundColor: 'white' }}>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>ID</TableCell>
-              <TableCell>Name</TableCell>
-              <TableCell>Laptops Required</TableCell>
-              <TableCell>Contact Number</TableCell>
-              <TableCell>Location of Operation</TableCell>
-              <TableCell>Purpose</TableCell>
-              <TableCell>Status</TableCell>
-              <TableCell>Edit Status</TableCell>
-              <TableCell>Delete</TableCell>
+      <TableContainer style={{ border: "none" }} sx={{ backgroundColor: 'white', mt: 2 }}>
+        <Table sx={{ border: "none" }}>
+          <TableHead sx={{ border: "none" }}>
+            <TableRow style={{ border: "none" }}>
+              <TableCell sx={classes.tableHeader}>ID</TableCell>
+              <TableCell sx={classes.tableHeader}>Name</TableCell>
+              <TableCell sx={classes.tableHeader}>Point of Contact</TableCell>
+              <TableCell sx={classes.tableHeader}>Contact Number</TableCell>
+              <TableCell sx={classes.tableHeader}>Laptop Required</TableCell>
+              <TableCell sx={classes.tableHeader}>Location of Operation</TableCell>
+              <TableCell sx={{ ...classes.tableHeader, width: "220px" }}>Purpose</TableCell>
+              <TableCell sx={classes.tableHeader}>Status</TableCell>
+              <TableCell sx={classes.tableHeader}>Delete</TableCell>
             </TableRow>
           </TableHead>
-          <TableBody>
+          <TableBody sx={classes.tablecell}>
             {filteredData.map((ngo) => (
-              <TableRow key={ngo.Id} hover >
-                <TableCell>{ngo.Id}</TableCell>
-                <TableCell>{ngo.organizationName}</TableCell>
-                <TableCell>{ngo.primaryContactName}</TableCell>
-                <TableCell>{ngo.contactNumber}</TableCell>
-                <TableCell>{ngo.location}</TableCell>
-                <TableCell>{ngo.expectedOutcome}</TableCell>
-
-                <TableCell>
-                  <FormControl fullWidth>
+              <TableRow key={ngo.Id} hover sx={classes.tablecell}>
+                <TableCell sx={classes.tablecell}>{ngo.Id}</TableCell>
+                <TableCell sx={classes.tablecell}>{ngo.organizationName}</TableCell>
+                <TableCell sx={classes.tablecell}>{ngo.primaryContactName}</TableCell>
+                <TableCell sx={classes.tablecell}>{ngo.contactNumber}</TableCell>
+                <TableCell sx={classes.tablecell}>{ngo.location}</TableCell>
+                <TableCell sx={classes.tablecell}>{ngo.expectedOutcome}</TableCell>
+                <TableCell sx={classes.tablecell}>
+                  <FormControl fullWidth >
+                    <InputLabel id="demo-simple-select-label">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="8" height="8" viewBox="0 0 8 8" fill="none">
+                        <circle cx="4" cy="4" r="4" fill="#FFAD33" />
+                      </svg>
+                      Request Submitted</InputLabel>
                     <Select
+                      labelId="demo-simple-select-label"
+                      id="demo-simple-select"
+                      label="Request Submitted"
                       value={ngo.Status}
                       onChange={(e) => handleStatusChange(ngo.Id, e.target.value)}
                     >
-                      {filterOptions.status.map(option => (
-                        <MenuItem key={option} value={option}>{option}</MenuItem>
+                      {filterOptions.status.map((option) => (
+                        <MenuItem key={option} value={option}>
+                          {option}
+                        </MenuItem>
                       ))}
                     </Select>
                   </FormControl>
-                </TableCell>
 
-               
-
-                <TableCell>
+                </TableCell >
+                <TableCell sx={{ border: "none" }}>
                   <IconButton onClick={() => handleDelete(ngo.Id)}><DeleteIcon /></IconButton>
                 </TableCell>
               </TableRow>
@@ -275,8 +297,20 @@ const AdminNgo = () => {
           </TableBody>
         </Table>
       </TableContainer>
-
-      {/* Confirmation Dialog */}
+      <Divider />
+      <TablePagination
+        sx={{ mt: 3, mb: 8 }}
+        rowsPerPageOptions={[10, 25, 100]}
+        component="div"
+        count={filteredData.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={(event, newPage) => setPage(newPage)}
+        onRowsPerPageChange={(event) => {
+          setRowsPerPage(parseInt(event.target.value, 10));
+          setPage(0);
+        }}
+      />
       <Dialog
         open={openDialog}
         onClose={handleCancelStatusChange}
@@ -294,7 +328,7 @@ const AdminNgo = () => {
           <Button onClick={handleConfirmStatusChange} color="primary" autoFocus>Confirm</Button>
         </DialogActions>
       </Dialog>
-    </Container>
+    </Container >
   );
 };
 
---
+++ src/Dashboard/AdminNgo/style.js
@@ -0,0 +1,22 @@
+import { border, fontSize, fontWeight } from "@mui/system";
+
+export const classes = {
+    tableHeader: {
+        fontSize: "16px",
+        fontWeight: "bold"
+    },
+    filter: {
+        mt: 3,
+        ml: 1,
+        fontSize: "16px",
+        fontWeight: "bold"
+    },
+    tablecell: {
+        border: "none"
+    },
+    FormControl: {
+        m: 1,
+        minWidth: 208,
+        width: { xs: "100%", sm: "100%", lg: 0, md: 0 }
+    }
+}
