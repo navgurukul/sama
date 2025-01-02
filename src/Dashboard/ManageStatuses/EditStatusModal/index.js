3c20a2760c8d97eb2a1698e29cd813963d219838
komalahire <komala@navgurukul.org>
2025-01-02 15:24:02 +0530

    changed api

--- src/Dashboard/ManageStatuses/EditStatusModal/index.js
+++ src/Dashboard/ManageStatuses/EditStatusModal/index.js
@@ -39,7 +39,7 @@ const handleSubmit = async (e) => {
     };
 
     await fetch(
-      "	https://script.google.com/macros/s/AKfycbyXgpBdyC423mHLQ-hm5vh9yRCHxzAD03Opk3Pc6x4Nik865xH6GJCVt-LYI_7i4UPA/exec?type=editManageStatus",
+      "https://script.google.com/macros/s/AKfycbxmnB0YHUm_mPxf1i-Cv465D1kSOrB0w1-dJS1slov_UQPZ0QxMERy_kZ8uZ5KASjBi/exec/exec?type=editManageStatus",
       {
         method: "POST",
         mode: "no-cors",
