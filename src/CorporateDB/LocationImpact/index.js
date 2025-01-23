bb07f53bcb4f6549b6b242f483b5940a6b41d6ca
komalahire <komala@navgurukul.org>
2025-01-23 12:30:51 +0530

    created corporate-db basic flow

---
+++ src/CorporateDB/LocationImpact/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Box, Typography } from "@mui/material";
+
+const LocationWiseImpact = () => {
+  return (
+    <div>
+      {" "}
+      <Box p={3}>
+        <Typography variant="h6">Location-Wise Impact</Typography>
+        {/* Your location impact content goes here */}
+      </Box>
+    </div>
+  );
+};
+
+export default LocationWiseImpact;
---
+++ src/CorporateDB/YearlyImpact/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Box, Typography } from "@mui/material";
+
+const YearlyImpact = () => {
+  return (
+    <div>
+      {" "}
+      <Box p={3}>
+        <Typography variant="h6">Yearly Impact</Typography>
+        {/* Your yearly impact content goes here */}
+      </Box>
+    </div>
+  );
+};
+
+export default YearlyImpact;
