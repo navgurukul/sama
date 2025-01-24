554f510caf62474d8a4468a295699a3596fa849f
roshni2003 <roshnijha21@navgurukul.org>
2025-01-24 15:17:06 +0530

    india-map-data

--- src/CorporateDB/LocationImpact/index.js
+++ src/CorporateDB/LocationImpact/index.js
@@ -1,21 +1,3 @@
-// import React from "react";
-// import { Box, Typography } from "@mui/material";
-
-// const LocationWiseImpact = () => {
-//   return (
-//     <div>
-//       {" "}
-//       <Box p={3}>
-//         <Typography variant="h6">Location-Wise Impact</Typography>
-//         {/* Your location impact content goes here */}
-//       </Box>
-//     </div>
-//   );
-// };
-
-// export default LocationWiseImpact;
-
-
 import React, { useEffect, useState } from 'react';
 import { MapContainer, GeoJSON } from 'react-leaflet';
 import { Typography, Box } from '@mui/material';
