1ea54551dd6b1f8271aefbdd102fa60f9ed68387
roshni2003 <roshnijha21@navgurukul.org>
2025-02-01 17:36:52 +0530

    our team page

---
+++ src/Pages/About/OurTeam/style.js
@@ -0,0 +1,48 @@
+const ourteam = {
+    container: {
+        textAlign: "center", 
+        py: 4, 
+        marginTop: "80px"
+    },
+    gridContainer: {
+        marginTop: "40px"
+    },
+    gridItem: {
+        display: "flex", 
+        justifyContent: "center"
+    },
+    card: {
+        width: "450px", 
+        height: "280px", 
+        display: "flex", 
+        alignItems: "center", 
+        justifyContent: "center", 
+        overflow: "hidden", 
+        borderRadius: "0px"
+    },
+    cardMedia: {
+        width: "100%", 
+        height: "100%", 
+        objectFit: "cover"
+    },
+    box: {
+        width: { xs: "100%", md: "726px" },
+        minHeight: "423px",
+        padding: { xs: "0px", md: "32px" },
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        gap: "16px"
+    },
+    typographyBody: {
+        textAlign: "left"
+    },
+    button: {
+        marginTop: "0px",
+        height: "48px",
+        width: { xs: "75%", md: "287px" },
+        fontSize: { xs: "14px", md: "17px" }
+    }
+};
+
+export default ourteam;
