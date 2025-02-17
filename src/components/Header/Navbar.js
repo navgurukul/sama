d4356071d06d78a23f9e85964cebffae8ced6412
komalahire <komala@navgurukul.org>
2025-02-17 16:08:41 +0530

    change heade name

--- src/components/Header/Navbar.js
+++ src/components/Header/Navbar.js
@@ -279,7 +279,7 @@ const Navbar = () => {
                     },
                   }}
                 >
-                  <Typography variant="subtitle1" sx={{ color: "primary.main", }}> Sumit your Requriment</Typography>
+                  <Typography variant="subtitle1" sx={{ color: "primary.main", }}> Submit your Requriment</Typography>
                 </Button>
               </MuiLink>
 
