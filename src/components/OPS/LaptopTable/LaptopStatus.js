9591c7bfdc14dd8621085c60d06dd82f7e8d6724
roshni2003 <roshnijha21@navgurukul.org>
2025-05-07 17:16:39 +0530

    status chnage

--- src/components/OPS/LaptopTable/LaptopStatus.js
+++ src/components/OPS/LaptopTable/LaptopStatus.js
@@ -1,36 +1,29 @@
 import React, {useEffect, useState} from 'react';
 import { Select, MenuItem, Checkbox } from '@mui/material';
 
-
-// export const LaptopStatusDropdown = ({ value, onChange }) => {
-//   return (
-//     <Select
-//       value={value || ''}
-//       onChange={onChange}
-//       displayEmpty
-//       style={{ borderRadius: "20px" }}
-//       fullWidth
-//     >
-//       <MenuItem value="Laptop Received">Laptop Received</MenuItem>
-//       <MenuItem value="Laptop Refurbished">Laptop Refurbished</MenuItem>
-//       <MenuItem value="To be dispatch">To Be Dispatch</MenuItem>
-//       <MenuItem value="Donated">Allocated</MenuItem>
-//     </Select>
-//   );
-// };
-
-
 export const LaptopStatusDropdown = ({ value = 'In Transit', onChange }) => {
   let options = [];
 
-  if (value === 'In Transit') {
-    options = ['Laptop Received'];
-  } else if (value === 'Laptop Received') {
-    options = ['Allocated'];
-  } else if (value === 'Allocated') {
-    options = ['Distributed'];
-  } else if (value === 'Distributed') {
-    options = ['Distributed']; // Locked final status
+  switch (value) {
+    case 'In Transit':
+      options = ['Laptop Received'];
+      break;
+    case 'Laptop Received':
+      options = ['Laptop Refurbished', 'To Be Dispatch'];
+      break;
+    case 'Laptop Refurbished':
+      options = ['To Be Dispatch'];
+      break;
+    case 'To Be Dispatch':
+      options = ['Allocated'];
+      break;
+    case 'Allocated':
+      options = ['Distributed'];
+      break;
+    case 'Distributed':
+    default:
+      options = []; // Final status
+      break;
   }
 
   return (
@@ -38,26 +31,23 @@ export const LaptopStatusDropdown = ({ value = 'In Transit', onChange }) => {
       value={value}
       onChange={onChange}
       displayEmpty
-      style={{ borderRadius: "20px" }}
+      style={{ borderRadius: '20px' }}
       fullWidth
     >
-      {/* Always show the current value as the selected option */}
+      {/* Always show the current value */}
       <MenuItem value={value}>{value}</MenuItem>
 
-      {/* Show valid next status only if it's different from current */}
-      {options.map(
-        (status) =>
-          status !== value && (
-            <MenuItem key={status} value={status}>
-              {status}
-            </MenuItem>
-          )
+      {/* Show valid next statuses if any */}
+      {options.map((status) =>
+        status !== value ? (
+          <MenuItem key={status} value={status}>
+            {status}
+          </MenuItem>
+        ) : null
       )}
     </Select>
   );
 };
-
-
 
 export const AssignedTo = ({ value, onChange }) => {
   return (
