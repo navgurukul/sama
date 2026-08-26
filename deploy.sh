983d9ae2e1a128051032b70f388c2c4dbf3ae89b
SAMA <product@thesama.in>
2026-08-26 11:17:02 +0530

    Fix Nginx config to allow /api routes

--- deploy.sh
+++ deploy.sh
@@ -303,6 +303,17 @@ server {
 
     # ---- FastAPI proxy endpoints ----
 
+    location /api {
+        proxy_pass         http://127.0.0.1:${BACKEND_PORT};
+        proxy_http_version 1.1;
+        proxy_set_header   Host \$host;
+        proxy_set_header   X-Real-IP \$remote_addr;
+        proxy_set_header   X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header   X-Forwarded-Proto \$scheme;
+        proxy_read_timeout 120s;
+        client_max_body_size 50M;
+    }
+
     location /exec {
         proxy_pass         http://127.0.0.1:${BACKEND_PORT};
         proxy_http_version 1.1;
